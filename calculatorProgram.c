5f70e3b85ff68e2b0b3fe63628ab0cae62d190b6
Marcio122 <marcio122@outlook.com.br>
2023-12-04 21:58:05 +0100

    first of the day

---
+++ calculatorProgram.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    char operator;
+    double num1;
+    double num2;
+    double result;
+
+    printf("\nEnter an operator(+ - * /): ");
+    scanf("%c", &operator);
+
+    printf("Enter number 1: ");
+    scanf("");
+
+    return 0;
+}
